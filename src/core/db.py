3e85a1cd25e7c9c60def9e3e6930c776ddb45af9
Erm2k8 <ermesson2k8and@gmail.com>
2025-05-05 11:40:58 -0300

    feat(config): initial setup for database code

--- src/core/db.py
+++ src/core/db.py
@@ -1,1 +1,9 @@
-# Arquivo destinado às ações do banco de dados
+from sqlmodel import Session, create_engine
+from .config import settings
+
+engine = create_engine(str(settings.DATABASE_SERVER))
+
+def init_db(session: Session) -> None:
+    # Tables should be created with Alembic Migrations
+
+    pass
