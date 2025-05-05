ad3bf02046462cd4db38a9820b6549010d79eb2c
Erm2k8 <ermesson2k8and@gmail.com>
2025-05-05 10:13:42 -0300

    feat: hello world route in main.py file

--- src/main.py
+++ src/main.py
@@ -1,1 +1,20 @@
-# Porta de entrada da aplicação, por aqui o sistema começará a rodar
+from fastapi import FastAPI
+
+app = FastAPI(
+    title='FISCALIZA',
+    version='0.1.0',
+)
+
+@app.get('/')
+async def hello_world():
+    return {'message': 'hello world'}
+
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run(
+        app=app,
+        host='localhost',
+        port=8000,
+    )
